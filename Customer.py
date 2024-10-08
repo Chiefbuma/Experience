bfbdb55fd2b3d5877a362dab17c3d3bede38b0e7
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-08 12:18:11 +0300

    EW

--- Customer.py
+++ Customer.py
@@ -11,19 +11,19 @@ from local_components import card_container
 from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
 from dateutil.relativedelta import relativedelta
 
-with card_container():
-    colm = st.columns([0.5,2,0.5])
-    with colm[1]:
-        # Display the image from the local folder
-        st.image("sunrise.jpg", caption="Sunrise by the mountains", width=200)
-    # Expander section with adjusted width
-        with st.expander("DOWNLOAD PREVIOUS MONTH"):
-            # Create a styled container for expander content
-            st.markdown(
-                """
-                <div style="width: 200px;">
-                    <p>Here you can download the data from the previous month.</p>
-                </div>
-                """, 
-                unsafe_allow_html=True
-            )
+
+colm = st.columns([0.5,2,0.5])
+with colm[1]:
+    # Display the image from the local folder
+    st.image("sunrise.jpg", caption="Sunrise by the mountains", width=200)
+# Expander section with adjusted width
+    with st.expander("DOWNLOAD PREVIOUS MONTH"):
+        # Create a styled container for expander content
+        st.markdown(
+            """
+            <div style="width: 200px;">
+                <p>Here you can download the data from the previous month.</p>
+            </div>
+            """, 
+            unsafe_allow_html=True
+        )
