f21175e77dc6de57b468a98d145171b6f2d255d9
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-08 12:08:21 +0300

    ew

--- Customer.py
+++ Customer.py
@@ -1,30 +1,15 @@
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
-from supabase import create_client, Client
 import pandas as pd
 from datetime import datetime, timedelta
 from IPython.display import display
 import calendar
 import numpy as np
-import plotly.express as px
 from IPython.display import HTML
-from office365.sharepoint.client_context import ClientContext
-from office365.runtime.auth.authentication_context import AuthenticationContext
 import streamlit_option_menu as option_menu
-import plotly.graph_objects as go
-import supabase
 import streamlit_shadcn_ui as ui
 from local_components import card_container
 from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
-import main
-from postgrest import APIError
 from dateutil.relativedelta import relativedelta
-
-
-import streamlit as st
-
-
-
 
 with card_container():
     colm = st.columns([0.05,2,0.05])
