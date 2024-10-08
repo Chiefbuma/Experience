8d075f3eb6f7b98e5ba82ff7a0d99a1ceef363a2
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-08 12:25:08 +0300

    ew

--- Customer.py
+++ Customer.py
@@ -21,7 +21,7 @@ with colm[1]:
         # Create a styled container for expander content
         st.markdown(
             """
-            <div style="width: 300px;">
+            <div style="width: 200px;">
                 <p>Here you can download the data from the previous month.</p>
             </div>
             """, 
